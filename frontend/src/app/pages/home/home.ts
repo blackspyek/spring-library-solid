aa0fe97e94a8d2463d50afc7c364bb7840ff0370
wyntia <gosia.chyrza@gmail.com>
2025-12-17 18:50:33 +0100

    Fixed search on the main page, fixed navbar

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -2,11 +2,23 @@ import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { PopularList } from '../../components/popular-list/popular-list';
 import { RecentList } from '../../components/recent-list/recent-list';
+import {Router} from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [MatIcon, PopularList, RecentList],
+  imports: [MatIcon, PopularList, RecentList, FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-export class Home {}
+export class Home {
+
+  searchTerm = '';
+
+  constructor(private router: Router) {}
+
+  onSearchClick(){
+    this.router.navigate(['/catalog'], {
+      queryParams: { q: this.searchTerm }});
+  }
+}
